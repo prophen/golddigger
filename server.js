7b8ad1982a102feb73334f79737a3e4cb0a7ffe0
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2026-01-13 13:33:55 -0800

    Add serveStatic, getContentType, and sendResponse utilities; update server and package.json

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import http from "node:http";
-
+import { serveStatic } from "./utils/serveStatic.js";
 const PORT = 8001;
 
-const __dirname = import.meta.__dirname;
+const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<html><h1>The server is working</h1></html>");
+  if (!req.url.startsWith("/api")) {
+    return await serveStatic(req, res, __dirname);
+  }
 });
 
 server.listen(PORT, () => console.log(`Connected on http://localhost:${PORT}`));
